f997d0cf2cd440cf9151515c608b804275993399
Tasha R Zuniga <Tashazun@gmail.com>
2018-03-01 21:38:59 -0800

    First solution

---
+++ code.js
@@ -0,0 +1,10 @@
+function Ship(draft,crew) {
+    this.draft = draft;
+    this.crew = crew;
+   }
+   
+   Ship.prototype.isWorthIt = function () {
+     const weight = (this.crew * 1.5);
+     const finalDraft = this.draft - weight;
+     return (finalDraft > 20 ? true : false);
+   };
