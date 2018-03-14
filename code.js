c9e8226e531565866bc85defe4aee5143f575ba5
Tasha R Zuniga <Tashazun@gmail.com>
2018-03-13 22:17:20 -0700

    Third kata finished

--- code.js
+++ code.js
@@ -13,3 +13,13 @@ function Ship(draft,crew) {
     const answer = name.split(' ').map(n => n.substring(0, 1)).join('.').toUpperCase();
     return answer;
   };
+
+  function cubeOdd(arr) {
+
+    const answer = arr.filter(x => x % 2 !== 0).map(x => (x * x) * x).reduce((a, b) => a + b);
+    if (isNaN(answer)) {
+      return undefined;
+    } else {
+    return answer;
+    }
+  }
